94920e5b2536c8c5accc41fb694ed1db78bac7b4
Rogelio Garcia <garciarogelio8991@gmail.com>
2020-08-08 10:47:14 -0700

    able to check if fields are empty else takes them to success page

--- scripts.js
+++ scripts.js
@@ -6,24 +6,42 @@ console.log("Js is linked");
   const username = document.getElementById("username");
   const pass = document.getElementById("pass");
   const pass2 = document.getElementById("pass2");
-//all of the divs that will tell user if there is an error
-  const emailError = document.getElementById("email-error");
+//all of the divs that will tell user if there is an error 
   const usernameError = document.getElementById("username-error");
+  const emailError = document.getElementById("email-error");
   const passError = document.getElementById("pass-error");
   const pass2Error = document.getElementById("pass2-error");
-  var message = "oh no";
+  var message = "";
+
 //function runs when user hits submit to check
 function checkIfValid() {
-  emailError.innerHTML = message;
-  usernameError.innerHTML = message;
-  passError.innerHTML = message;
-  pass2Error.innerHTML = message;
-
-  //stops the default action of submiting, and erasing users info they in text field
   event.preventDefault();
 
+if(email.value.length == 0){
+  message = "E-mail is empty, please enter email";
+  emailError.innerHTML = message;
+}
+if(username.value.length == 0){
+  message = "username is empty, please enter a username";
+  usernameError.innerHTML = message;
+}
+if(pass.value.length == 0){
+  message = "password is empty, please enter a password";
+  passError.innerHTML = message;
+}
+if(pass2.value.length == 0){
+  message = "password is empty, please enter a password";
+  pass2Error.innerHTML = message;
+}
+  else{
+    window.open('success.html', '_blank');
+  }
 
-  //window.open('success.html', '_blank');
+  //stops the default action of submiting, and erasing users info they in text field
+  
+
+
+  
 }
 
 //user presses the submit button and runs the function to check
